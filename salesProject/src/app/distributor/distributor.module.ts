315544bd8768b5206dfc10ad9b9a74852c9cab86
praveenreddy227 <111289609+praveenreddy227@users.noreply.github.com>
2022-12-23 11:26:11 +0530

    dcp

--- salesProject/src/app/distributor/distributor.module.ts
+++ salesProject/src/app/distributor/distributor.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { ProductsComponent } from './products/products.component';
+import { PurchaceOrderComponent } from './purchace-order/purchace-order.component';
+import { StockComponent } from './stock/stock.component';
+import { RaiseNewComponent } from './purchace-order/raise-new/raise-new.component';
+import { ViewAllComponent } from './purchace-order/view-all/view-all.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MyProfileComponent,
+    ProductsComponent,
+    PurchaceOrderComponent,
+    StockComponent,
+    RaiseNewComponent,
+    ViewAllComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ salesProject/src/app/distributor/purchace-order/raise-new/raise-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raise-new',
+  templateUrl: './raise-new.component.html',
+  styleUrls: ['./raise-new.component.css']
+})
+export class RaiseNewComponent {
+
+}
