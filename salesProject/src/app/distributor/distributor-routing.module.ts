2e0bfb961d43320499a6aeef469eb716fa99d19b
praveenreddy227 <111289609+praveenreddy227@users.noreply.github.com>
2022-12-24 12:15:54 +0530

    distributor changes 1

--- salesProject/src/app/distributor/distributor-routing.module.ts
+++ salesProject/src/app/distributor/distributor-routing.module.ts
@@ -1,37 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
+
 import { DistributorComponent } from './distributor.component';
-=======
->>>>>>> c34f036e723a5054fb2a8612aee583c5ca76d3c7
->>>>>>> Stashed changes
+
+
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { ProductsComponent } from './products/products.component';
 import { PurchaceOrderComponent } from './purchace-order/purchace-order.component';
 import { StockComponent } from './stock/stock.component';
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 const routes: Routes = [{path:'',component:DistributorComponent},
 {path:'stock',component:StockComponent},
 {path:'my-profile',component:MyProfileComponent},
 {path:'purchase-order',component:PurchaceOrderComponent},
 {path:'products',component:ProductsComponent}
-=======
->>>>>>> Stashed changes
-const routes: Routes = [
-  { path: 'profile', component: MyProfileComponent },
-  { path: 'products', component: ProductsComponent },
-  {path:'purchase',component:PurchaceOrderComponent },
-  {path:'stock',component:StockComponent}
 
-<<<<<<< Updated upstream
-=======
->>>>>>> c34f036e723a5054fb2a8612aee583c5ca76d3c7
->>>>>>> Stashed changes
 ];
 
 @NgModule({
