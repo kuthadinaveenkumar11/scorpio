4ce5f897fbae6bd6fd0b2b7c828b08e4e1538b9f
Naveen Kumar <kuthadinaveenkumar11@gmail.com>
2022-12-26 11:50:30 +0530

    change

--- salesProject/src/app/login/login.component.ts
+++ salesProject/src/app/login/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent {
   currentDate=new Date();
   changing:string="Select The User Type";
   passwordchanging:string="Select The User Type";
+  disabled=true;
   
   loginform=new FormGroup({
     Uname:new FormControl(''),
@@ -20,38 +21,87 @@ export class LoginComponent {
   constructor(private routee:Router) { }
 
   ngOnInit(): void {
+
   }
   onClick(){
-    if(this.loginform.value.dis=="Distu")
+    console.log(this.loginform.value);
+
+    if(this.loginform.value.dis=="Distributorvalue")
     {
-      if(this.loginform.value.Uname=='naveen11'&&this.loginform.value.Pssword=='12345')
+      console.log(this.loginform.value.dis);
+
+      if(this.loginform.value.Uname=='Dis'&&this.loginform.value.Pssword=='12345')
+    {
+    this.routee.navigate(['Distributor']);
+    }
+    else{
+    alert("enter crt");
+    }
+    }
+    else if (this.loginform.value.dis=="Authorizervalue"){
+      if(this.loginform.value.Uname=='auth'&&this.loginform.value.Pssword=='12345')
+    {
+    this.routee.navigate(['Authorizer']);
+    }
+    else{
+    alert("enter crt");
+    }
+    }
+    else if (this.loginform.value.dis=="warehousevalue"){
+      if(this.loginform.value.Uname=='ware'&&this.loginform.value.Pssword=='12345')
+      {
+        this.routee.navigate(['Warehouse']);
+      }
+      else{
+        alert("enter crt");
+      }
+    }
+    else if (this.loginform.value.dis=="adminvalue"){
+      if(this.loginform.value.Uname=='admin'&&this.loginform.value.Pssword=='12345')
     {
     this.routee.navigate(['Admin']);
     }
     else{
-      console.log(this.loginform.value);
     alert("enter crt");
     }
     }
     else{
-    alert("Select the user ")}
+    alert("Select the user ")
+    }
   }
   Distributor(){
       this.changing="Enter the Distributor Mail";
       this.passwordchanging="Enter the Distributor Password";
-      this.loginform.value.dis="Distu";
+      this.loginform.value.dis="Distributorvalue";
+      this.disabled=false;
+      console.log(this.loginform.value);
+
+
+
   }
   Authorizer(){
     this.changing="Enter the Authorizer Mail";
     this.passwordchanging="Enter the Authorizer Password";
+    this.disabled=true;
+    this.loginform.value.dis="Authorizervalue";
+
+
   }
   Warehouse(){
     this.changing="Enter the Warehouse Mail";
     this.passwordchanging="Enter the Warehouse Password";
+    this.disabled=true;
+    this.loginform.value.dis="warehousevalue";
+
+
   }
   Admin(){
     this.changing="Enter the Admin Mail";
     this.passwordchanging="Enter the Admin Password";
+    this.disabled=true;
+    this.loginform.value.dis="adminvalue";
+
+
   }
 }
   
