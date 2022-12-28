d88e54f8eb0a229d8d26f17a8724878809ac9791
Naveen Kumar <kuthadinaveenkumar11@gmail.com>
2022-12-28 13:15:29 +0530

    xz

--- Projects_Web_Api/Model1.Designer.cs
+++ Projects_Web_Api/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\sorpio\Projects_Web_Api\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\Project\scorpio\Projects_Web_Api\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
