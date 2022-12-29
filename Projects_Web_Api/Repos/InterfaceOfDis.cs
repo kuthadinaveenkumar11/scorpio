834da1aee3dee4f9438ac463f4a93ea8fbdb191b
Naveen Kumar <kuthadinaveenkumar11@gmail.com>
2022-12-29 12:10:43 +0530

    yjnyj

--- Projects_Web_Api/Repos/InterfaceOfDis.cs
+++ Projects_Web_Api/Repos/InterfaceOfDis.cs
@@ -15,7 +15,8 @@ namespace Projects_Web_Api.Repos
     }
     public class ClassofDis : InterfaceOfDis
     {
-        scorpioDBEntities obj = new scorpioDBEntities();
+        //scorpioDBEntities obj = new scorpioDBEntities();
+        EcomDBEntities obj = new EcomDBEntities();
         Model_Of_Drisributer_Profile InterfaceOfDis.Get_Of_Drisributer_Profile(string PAN)
         {
             var dis = obj.distributer_profiles.Where(n=>n.PANNo==PAN).Select(s => new Model_Of_Drisributer_Profile()
